7c6eb4d99a41c7a7e68295b4702a16f2643a8416
jongwonlee00 <jole2550@colorado.edu>
2023-11-15 16:23:20 -0700

    reverted to liz

--- Project Code/src/index.js
+++ Project Code/src/index.js
@@ -222,14 +222,10 @@ app.get('/logout', (req, res) => {
       }
     });
   });
-
-  app.get('/welcome', (req, res) => {
-    res.json({status: 'success', message: 'Welcome!'});
-  });
 // *****************************************************
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
-module.exports = app.listen(3000);
+app.listen(3000);
 console.log('Server is listening on port 3000');
 
